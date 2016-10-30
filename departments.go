5364787432aae003f2af35e3e80d6ae7388e1779
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-10-30 16:54:43 -0400

    Add handler for fetching departments from the API.

---
+++ departments.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+const (
+	departmentsURL = "https://ssb.cc.nd.edu/StudentRegistrationSsb/ssb/classSearch/get_subject?searchTerm=&term=201620&offset=1&max=200"
+)
+
+type Department struct {
+	Code        string
+	Description string
+}
+
+func fetchDepartments() ([]Department, error) {
+	fmt.Println("Starting to fetch departments")
+
+	client, err := setupClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := doGet(client, departmentsURL)
+	departments := make([]Department, 0)
+	err = json.NewDecoder(strings.NewReader(r)).Decode(&departments)
+
+	if err != nil {
+		fmt.Println("Someting went wrong!")
+		return nil, err
+	}
+
+	fmt.Println("Done fetching departments!")
+
+	return departments, err
+}
