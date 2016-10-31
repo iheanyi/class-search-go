9a67e733879e6eca446796f4792f2fe9666b739b
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-10-30 20:07:58 -0400

    Add a struct ofr instructors and a method for fetching them.

---
+++ instructors.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type Instructor struct {
+	BannerId string `json:"code"`
+	Name     string `json:"description"`
+}
+
+const (
+	instructorURL = "https://ssb.cc.nd.edu/StudentRegistrationSsb/ssb/classSearch/get_instructor?searchTerm=&offset=1&max=10000&term="
+)
+
+func FetchInstructors(t *Term) ([]Instructor, error) {
+	fmt.Println("Starting to fetch instructors!")
+	client, err := setupClient()
+
+	if err != nil {
+		return nil, err
+	}
+
+	fullSearchURL := instructorURL + t.Code
+
+	authenticateClient(client, t.Code)
+	fmt.Println(fullSearchURL)
+
+	r, err := doGet(client, fullSearchURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	instructors := make([]Instructor, 0)
+	err = json.NewDecoder(strings.NewReader(r)).Decode(&instructors)
+
+	if err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Done fetching instructors!")
+	return instructors, err
+}
