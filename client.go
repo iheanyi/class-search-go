813a876d5beff795654159f3c1b44f5c87d36bf1
Iheanyi Ekechukwu <iheanyi@digitalocean.com>
2016-11-03 18:10:07 -0400

    Begin refactor for API CLient to use a Client Struct and Services.

--- client.go
+++ client.go
@@ -1,16 +1,101 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
+	"net/url"
 )
 
 const (
+	defaultBaseURL  = "https://ssb.cc.nd.edu/"
 	baseAuthTermURL = "https://ssb.cc.nd.edu/StudentRegistrationSsb/ssb/term/search?mode=search&term="
 )
+
+type Client struct {
+	client  *http.Client
+	BaseURL *url.URL
+
+	Terms *TermsService
+}
+
+func NewClient(httpClient *http.Client) (*Client, error) {
+	if httpClient == nil {
+		cookieJar, err := cookiejar.New(nil)
+		if err != nil {
+			return nil, err
+		}
+
+		httpClient = &http.Client{
+			Jar: cookieJar,
+		}
+	}
+
+	baseURL, _ := url.Parse(defaultBaseURL)
+
+	c := &Client{client: httpClient, BaseURL: baseURL}
+	c.Terms = &TermsService{client: c}
+
+	return c, nil
+}
+
+func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Request, error) {
+	rel, err := url.Parse(urlStr)
+	if err != nil {
+		return nil, err
+	}
+
+	u := c.BaseURL.ResolveReference(rel)
+
+	buf := new(bytes.Buffer)
+	if body != nil {
+		err := json.NewEncoder(buf).Encode(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	req, err := http.NewRequest(method, u.String(), buf)
+	if err != nil {
+		return nil, err
+	}
+
+	return req, nil
+}
+
+func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
+	resp, err := c.client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		if rerr := resp.Body.Close(); err == nil {
+			err = rerr
+		}
+	}()
+
+	if v != nil {
+		if w, ok := v.(io.Writer); ok {
+			_, err = io.Copy(w, resp.Body)
+			if err != nil {
+				return nil, err
+			}
+		} else {
+			err := json.NewDecoder(resp.Body).Decode(v)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return resp, err
+}
 
 func setupClient() (*http.Client, error) {
 	cookieJar, err := cookiejar.New(nil)
---
+++ terms_service.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"net/http"
+)
+
+const (
+	termsBasePath = "StudentRegistrationSsb/ssb/classSearch/getTerms"
+)
+
+type TermsService struct {
+	client *Client
+}
+
+func (ts *TermsService) List() ([]Term, *http.Response, error) {
+	path := termsBasePath
+
+	req, err := ts.client.NewRequest("GET", path, nil)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	q := req.URL.Query()
+	q.Set("searchTerm", "")
+	q.Set("offset", "1")
+	q.Set("max", "10")
+	req.URL.RawQuery = q.Encode()
+
+	var terms []Term
+	res, err := ts.client.Do(req, terms)
+
+	if err != nil {
+		return nil, nil, err
+	}
+	return terms, res, nil
+}
