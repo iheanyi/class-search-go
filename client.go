4599c28bb249766812decf43f7966e641660e318
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2016-11-05 16:28:44 -0400

    Remove logging.

--- client.go
+++ client.go
@@ -68,7 +68,6 @@ func (c *Client) NewRequest(method, urlStr string, body interface{}) (*http.Requ
 }
 
 func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
-	log.Printf("url=%q", req.URL)
 	resp, err := c.client.Do(req)
 	if err != nil {
 		return nil, err
@@ -89,7 +88,6 @@ func (c *Client) Do(req *http.Request, v interface{}) (*http.Response, error) {
 		} else {
 			buf := bytes.NewBuffer(nil)
 			io.Copy(buf, resp.Body)
-			log.Print(buf.String())
 			err := json.NewDecoder(buf).Decode(v)
 			if err != nil {
 				return nil, err
